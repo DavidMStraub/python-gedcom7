20729452103dbd74435b10d0e722e8d918c11812
David Straub <straub@protonmail.com>
2022-10-09 19:51:32 +0200

    Add test

--- test/test_parser.py
+++ test/test_parser.py
@@ -4,6 +4,22 @@ GEDCOM_MIN = """0 HEAD
 0 TRLR
 """
 
+GEDCOM_EXTTAG = """0 HEAD
+1 GEDC
+2 VERS 7.0.0
+1 SCHMA
+2 TAG _FOO http://example.com/placeholder
+0 @I1@ INDI
+1 ALIA @I2@
+1 _FOO 23
+0 @I2@ INDI
+1 NAME
+2 PART Tom Jones
+1 ASSO @I1@
+2 TYPE GODP
+0 TRLR
+"""
+
 import pathlib
 
 import gedcom7
@@ -17,7 +33,7 @@ def test_minimal():
     assert len(record.children) == 1
     assert record.children[0].parent == record
     assert len(record.children[0].children) == 1
-    assert record.children[0].children[0].linestr == "7.0"
+    assert record.children[0].children[0].text == "7.0"
     record = records[1]
     assert record.tag == "TRLR"
     assert len(record.children) == 0
@@ -26,3 +42,8 @@ def test_maximal():
     filename = pathlib.Path(__file__).parent / "data" / "maximal70.ged"
     with open(filename, encoding="utf-8") as f:
         gedcom7.loads(f.read())
+
+def test_exttag():
+    records = gedcom7.loads(GEDCOM_EXTTAG)
+    assert len(records) == 4
+    assert records[1].children[1].tag == "http://example.com/placeholder"
