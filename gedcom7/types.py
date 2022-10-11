c4cb1e56f49e22c775cdf40565cd4d292a9a545b
David Straub <straub@protonmail.com>
2022-10-11 21:10:29 +0200

    Completely redo types

--- gedcom7/types.py
+++ gedcom7/types.py
@@ -1,7 +1,7 @@
 """Classes and data types."""
 
 import re
-from typing import Optional, List
+from typing import Any, Dict, Optional, List, Union
 
 from . import grammar
 
@@ -36,31 +36,110 @@ class GedcomStructure:
             f"children={repr_children}"
         )
 
-    def _match_type(self, regex, name: str):
-        """Check if content matches type."""
-        match = re.fullmatch(regex, self.text)
+    def as_type(self, dtype: str):
+        """Return as data type."""
+        dtype_cls = globals()[dtype]
+        return dtype_cls(self.text).parse()
+
+
+class DataType:
+    """Gedcom data type."""
+
+    REGEX = ""
+
+    def __init__(self, text: str) -> None:
+        """Initialize with a string."""
+        self.text = text
+        self.match = self._match()
+
+    @property
+    def name(self) -> str:
+        """Name of the type."""
+        return self.__class__.__name__
+
+    def __repr__(self):
+        return f"{self.name}(\"{self.text}\")"
+
+    def _match(self) -> re.Match:
+        """Match a string and raise if not compatible."""
+        match = re.fullmatch(self.REGEX, self.text)
         if not match:
-            raise ValueError(f"Cannot interpret {self.text} as type {name}")
+            raise ValueError(f"Cannot interpret {self.text} as type {self.name}")
         return match
 
-    def as_list_enum(self) -> List[str]:
-        """Return as list enum data type."""
-        self._match_type(grammar.list_enum, "list enum")
-        return [el.strip() for el in self.text.split(",")]
-
-    def as_enum(self) -> List[str]:
-        """Return as list enum data type."""
-        self._match_type(grammar.enum, "enum")
+    def parse(self):
+        """Parse the string."""
         return self.text
 
-    def as_personal_name(self) -> List[str]:
-        """Return as personal name data type."""
-        match = self._match_type(grammar.personalname, "personal name")
-        full_name = self.text.replace("/", "")
-        surname = match.group("surname")
-        return full_name, surname
 
-    def as_integer(self) -> List[str]:
-        """Return as integer data type."""
-        self._match_type(grammar.integer, "integer")
+class Integer(DataType):
+    """Integer type."""
+
+    REGEX = grammar.integer
+
+    def parse(self):
+        """Parse the string."""
         return int(self.text)
+
+
+class PersonalName(DataType):
+    """Personal name type."""
+
+    REGEX = grammar.personalname
+
+    def parse(self) -> Dict[str, Optional[str]]:
+        """Parse the string."""
+        fullname = self.text.replace("/", "")
+        surname = self.match.group("surname")
+        return {"fullname": fullname, "surname": surname}
+
+
+class Time(DataType):
+    """Time type."""
+
+    REGEX = grammar.time
+
+    def parse(self) -> Dict[str, Optional[Union[str, int]]]:
+        """Parse the string."""
+        match = self.match.groupdict()
+
+        def f(k, v):
+            if k == "tz":
+                return v
+            if v is None:
+                return v
+            return int(v)
+
+        return {k: f(k, v) for k, v in match.items()}
+
+
+class Age(DataType):
+    """Age type."""
+
+    REGEX = grammar.age
+
+    def parse(self) -> Dict[str, Any]:
+        """Parse the string."""
+        match = self.match.groupdict()
+        res = {
+            "agebound": match["agebound"],
+            "years": match["years"],
+            "months": match["months1"] or match["months2"],
+            "weeks": match["weeks1"] or match["weeks2"] or match["weeks3"],
+            "days": match["days1"] or match["days2"] or match["days3"] or match["days4"],
+        }
+        def f(k, v):
+            if k == "agebound":
+                return v
+            return int(v[:-1])
+        return {k: f(k, v) for k, v in res.items() if v is not None}
+
+
+class ListEnum(DataType):
+    """List Enum type."""
+
+    REGEX = grammar.list_enum
+
+    def parse(self) -> List[str]:
+        """Parse the string."""
+        return [el.strip() for el in self.text.split(",")]
---
+++ test/test_types.py
@@ -0,0 +1,85 @@
+"""Tests Gedcom types."""
+
+import pytest
+
+import gedcom7.types as T
+
+
+def test_integer():
+    with pytest.raises(ValueError):
+        T.Integer("a")
+    with pytest.raises(ValueError):
+        T.Integer(" 1")
+    assert T.Integer("203").parse() == 203
+
+
+def test_as_type():
+    struct = T.GedcomStructure("_SOMETAG", "", "15", "")
+    assert struct.as_type("Integer") == 15
+
+
+def test_personalname():
+    assert T.PersonalName("John Doe").parse() == {
+        "fullname": "John Doe",
+        "surname": None,
+    }
+    assert T.PersonalName("John /Doe/").parse() == {
+        "fullname": "John Doe",
+        "surname": "Doe",
+    }
+
+
+def test_time():
+    with pytest.raises(ValueError):
+        T.Time("13.15")
+    with pytest.raises(ValueError):
+        T.Time("13:15.2")
+    with pytest.raises(ValueError):
+        T.Time("13:15A")
+    with pytest.raises(ValueError):
+        T.Time("25:01")
+    with pytest.raises(ValueError):
+        T.Time("23:01:71")
+    with pytest.raises(ValueError):
+        T.Time("23:61")
+    assert T.Time("13:15").parse() == {
+        "hour": 13,
+        "minute": 15,
+        "second": None,
+        "fraction": None,
+        "tz": None,
+    }
+    assert T.Time("13:15:12").parse() == {
+        "hour": 13,
+        "minute": 15,
+        "second": 12,
+        "fraction": None,
+        "tz": None,
+    }
+    assert T.Time("13:15:12.246").parse() == {
+        "hour": 13,
+        "minute": 15,
+        "second": 12,
+        "fraction": 246,
+        "tz": None,
+    }
+    assert T.Time("13:15Z").parse() == {
+        "hour": 13,
+        "minute": 15,
+        "second": None,
+        "fraction": None,
+        "tz": "Z",
+    }
+
+
+def test_age():
+    with pytest.raises(ValueError):
+        T.Age("3Y")
+    assert T.Age("3y").parse() == {"years": 3}
+    assert T.Age("> 3y").parse() == {"years": 3, "agebound": ">"}
+    assert T.Age("3y 6m 5d").parse() == {"years": 3, "months": 6, "days": 5}
+
+
+def test_list_enum():
+    assert T.ListEnum("FOO").parse() == ["FOO"]
+    assert T.ListEnum("FOO, BAR").parse() == ["FOO", "BAR"]
